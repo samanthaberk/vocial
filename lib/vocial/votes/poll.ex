30956f1670319a07c895e7dba6a41e49b8adb98b
Samantha Berk <sberk10@gmail.com>
2019-02-09 13:19:35 -0800

    Added option schema with changeset and set up relationship between options and polls"

--- lib/vocial/votes/poll.ex
+++ lib/vocial/votes/poll.ex
@@ -2,10 +2,12 @@ defmodule Vocial.Votes.Poll do
   use Ecto.Schema
   import Ecto.Changeset
   alias Vocial.Votes.Poll
+  alias Vocial.Votes.Option
 
   schema "polls" do
     field :title, :string
 
+    has_many :options, Option
     timestamps()
   end
 
